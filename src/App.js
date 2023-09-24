586594fec33b54babd47961b847e1ea1035d9ae3
Krishang Patel <kdp8@njit.edu>
2023-09-24 03:18:38 -0400

    First Working Feature (Top 5 Rented Movies)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import './App.css';
 import React from 'react';
-import ActorList from './components/home';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from './components/home';
+import Customer from './components/customers';
+import NotFound from './components/notFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <ActorList/>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/customers">Customers</Link>
+            </li>
+            <li>
+              <Link to="/movies">movies</Link>
+            </li>
+            <li>
+              <Link to="/report">report</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/customers" element={<Customer />} />
+          <Route path="/movies" element={<Home />} />
+          <Route path="/report" element={<Home />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
-
-
-
-
-
-
-
-
-
-// import HelloWorld from './HelloWorld';
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <HelloWorld />
-//     </div>
-//   );
-// }
-// export default App;
-
-
--- src/components/home.js
+++ src/components/home.js
@@ -1,32 +1,51 @@
-// src/components/ActorList.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-function ActorList() {
-  const [actors, setActors] = useState([]);
+function Home() {
+  const [movieData, setMovieData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/home/')
-      .then(response => {
-        setActors(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+  const fetchMovieData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/top-rented-movies/');
+      const data = response.data;
+      setMovieData(data);
+      setIsModalOpen(true);
+    } catch (error) {
+      console.error('Error fetching movie data:', error);
+    }
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
-      <h1>Actor List</h1>
-      <ul>
-        {actors.map(actor => (
-          <ol key={actor.actor_id}>
-            {actor.first_name} {actor.last_name}
-          </ol>
-        ))}
-      </ul>
+      <div>
+        Buttons:
+        <button onClick={fetchMovieData}>
+          Top 5 Rented Movies
+        </button>
+        <button>
+          Top 5 Actors by No. of Films
+        </button>
+      </div>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Top 5 Rented Movies</h2>
+            <button onClick={handleModalClose}>OK</button>
+            <ul>
+              {movieData.map((movie, index) => (
+                <ol>{`${index + 1}. ${movie.title}`}</ol>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
-export default ActorList;
+export default Home;
