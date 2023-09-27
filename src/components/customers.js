9a036be85a8d7e6d5e00379fe54e85576dcd809f
Krishang Patel <kdp8@njit.edu>
2023-09-27 00:34:38 -0400

    Working Customer list (Filter/View List)

--- src/components/customers.js
+++ src/components/customers.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Customer() {
+  const [customers, setCustomers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showAll, setShowAll] = useState(false);
+  const [originalCustomers, setOriginalCustomers] = useState([]);
+  const [noResults, setNoResults] = useState(false);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/customers/')
+      .then((response) => {
+        setCustomers(response.data);
+        setOriginalCustomers(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleSearch = () => {
+    const filteredCustomers = originalCustomers.filter((customer) =>
+      customer.customer_id.toString().includes(searchQuery) ||
+      customer.first_name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      customer.last_name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    if (filteredCustomers.length === 0) {
+      setNoResults(true); 
+    } else {
+      setNoResults(false);
+    }
+    setCustomers(filteredCustomers);
+    setShowAll(false);
+  };
+
+  const handleShowAll = () => {
+    setCustomers(originalCustomers);
+    setSearchQuery('');
+    setShowAll(true);
+    setNoResults(false);
+  };
+
   return (
     <div>
-      <h1>Customers Page</h1>
+      <h1>Customer List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Search by ID, first name, or last name"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+        {showAll && <button onClick={handleShowAll}>Show All</button>}
+      </div>
+      {noResults ? (
+        <p>No results found.</p>
+      ) : (
+        <ul>
+          {customers.map((customer) => (
+            <li key={customer.customer_id}>
+              {customer.customer_id}: {customer.first_name} {customer.last_name}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
--- src/components/movies.js
+++ src/components/movies.js
@@ -1,16 +1,16 @@
-// movies.js
-
 import React, { useState } from 'react';
 
 function Movies() {
   const [searchQuery, setSearchQuery] = useState('');
   const [movies, setMovies] = useState([]);
+  const [searched, setSearched] = useState(false);
 
   const handleSearch = async () => {
     try {
       const response = await fetch(`http://127.0.0.1:8000/film-search/?search=${searchQuery}`);
       const data = await response.json();
       setMovies(data);
+      setSearched(true);
     } catch (error) {
       console.error('Error searching for movies:', error);
     }
@@ -26,13 +26,17 @@ function Movies() {
         onChange={(e) => setSearchQuery(e.target.value)}
       />
       <button onClick={handleSearch}>Search</button>
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.film_id}>
-            {movie.title} - {movie.description}
-          </li>
-        ))}
-      </ul>
+      {searched && movies.length === 0 ? (
+        <p>No results found.</p>
+      ) : (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.film_id}>
+              {movie.title} - {movie.description}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
